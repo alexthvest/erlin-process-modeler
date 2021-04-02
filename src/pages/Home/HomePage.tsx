b954d9b3efeb230edd592c3722bda085db5d74a9
alexthvest <alexthvest@gmail.com>
2021-04-02 19:53:49 +0300

    feat: add octicons

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { RepoIcon } from "@primer/octicons-react";
 
 export const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex flex-col items-center pt-16 space-y-2">
+      <h2 className="text-xl font-semibold">Добро пожаловать!</h2>
+      <p>Чтобы создать новый проект - ждите новых обновлений</p>
+      <br />
+      <button>
+        <RepoIcon className="mr-2" />
+        Новый проект
+      </button>
     </div>
   );
 };
